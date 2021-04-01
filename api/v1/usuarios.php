2e946eafe55f54bcc5f84ffc3ec320d15467cfcd
Daniel Rodríguez <darm1609@gmail.com>
2021-04-01 10:46:58 -0500

    01/04/2021 1

--- api/v1/usuarios.php
+++ api/v1/usuarios.php
@@ -1,6 +1,6 @@
 <?php
-    require_once 'vendor/autoload.php';
-    require_once 'auth.php';
+    require_once("vendor/autoload.php");
+    require_once("auth.php");
     require_once("config.php");
     require_once("librerias/basedatos.php");
 
