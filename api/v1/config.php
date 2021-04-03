79e783428ea3d05800da861b13bf421d5fb8e4dc
Daniel Rodríguez <darm1609@gmail.com>
2021-04-02 21:24:28 -0500

    cambio 02/04/2021 noche

--- api/v1/config.php
+++ api/v1/config.php
@@ -4,12 +4,15 @@
 	global $usuario;
 	global $pass;
 	global $basedatos;
+	global $clienteId;
+	global $rutaDeAdjuntos;
 	$servidor='localhost';
 	$puerto='3306';
 	$usuario='root';
 	$pass='';
 	$basedatos='dr';
 	$clienteId=1;
+	$rutaDeAdjuntos="adjuntos/";
 	ini_set("display_errors","On");
 	date_default_timezone_set("America/Caracas");
 ?>
